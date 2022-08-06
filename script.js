a8b6dda442b921d95f8523cb8d7528a840c50033
rohit4242 <rohitluni4242@gmail.com>
2022-08-06 16:23:28 +0530

    make button workeble

---
+++ script.js
@@ -0,0 +1,28 @@
+function startStop()
+{
+    let btn = document.getElementById('start-stop');
+    let status = document.getElementById('motor-status');
+
+    if(btn.innerHTML === "START"){
+        btn.innerHTML = "STOP";
+        status.innerHTML = "ON";
+        status.style = "color: green;";
+      
+
+    }
+    else{
+        btn.innerHTML = "START";
+        status.innerHTML = "OFF";
+        status.style = "color: red;";
+    }
+}
+
+function mode(){
+    var x = document.getElementById("card-cp");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+
+  }
+}
