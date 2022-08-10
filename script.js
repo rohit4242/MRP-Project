d068d57eebea0886f512c713f932cc5529e2489e
rohit4242 <rohitluni4242@gmail.com>
2022-08-10 12:29:39 -0700

    init

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function startStop()
     let btn = document.getElementById('start-stop');
     let status = document.getElementById('motor-status');
 
-    if(btn.innerHTML === "START"){
-        btn.innerHTML = "STOP";
+    if(btn.innerHTML === "Force Start"){
+        btn.innerHTML = "Force Stop";
         status.innerHTML = "ON";
-        status.style = "color: green;";
+        status.style = "color: #00cc99;";
     }
     else{
-        btn.innerHTML = "START";
+        btn.innerHTML = "Force Start";
         status.innerHTML = "OFF";
         status.style = "color: red;";
     }
