92f9acb23a70f82ba650bd14e52297fe82c9d4f4
rohit4242 <rohitluni4242@gamil.com>
2022-08-13 03:00:15 +0530

    init

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function startStop()
 {
     let btn = document.getElementById('start-stop');
@@ -15,12 +16,43 @@ function startStop()
     }
 }
 
-function mode(){
-    var x = document.getElementById("card-cp");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
+function switchMode(){
+  let btn = document.getElementById('switch-mode');
+  let range1 = document.getElementById("range1");
+  let range2 = document.getElementById("range2");
+  let range3 = document.getElementById("range3");
+  let cp = Array.from(document.getElementById('cp')) 
+  if(btn.innerHTML === "Switch To Manual Mode")
+  {
+    btn.innerHTML = "Switch To Automatic Mode";
+    document.getElementById('current-preset').innerHTML = "Recommended Preset";
+    range1.disabled = true;
+    range2.disabled = true;
+    range3.disabled = true;
+    range1.style.display = 'none';
+    range2.style.display = 'none';
+    range3.style.display = 'none';
+  }
+  else{
+    btn.innerHTML = "Switch To Manual Mode";
+    document.getElementById('current-preset').innerHTML = "Current Preset";
+    range1.disabled = false;
+    range2.disabled = false;
+    range3.disabled = false;
+    range1.style.display = 'block';
+    range2.style.display = 'block';
+    range3.style.display = 'block';
 
   }
+}
+
+
+function temp_rs(value) {
+  document.getElementById('var-temp').innerHTML = value;
+}
+function soil_Moisture_rs(value) {
+  document.getElementById('var-soil-moisture').innerHTML = value;
+}
+function humidity_rs(value) {
+  document.getElementById('var-humidity').innerHTML = value;
 }
