07678241b5f35732a0c556adcd3e94d5aa37136c
rohit4242 <rohitluni4242@gmail.com>
2022-08-23 10:42:42 +0530

    fetch live data from the server using fetch api

---
+++ fetchLiveData.js
@@ -0,0 +1,40 @@
+
+let temp_live_data = document.getElementById("temp_live_data");
+let hum_live_data = document.getElementById("hum_live_data");
+let soil_live_data = document.getElementById("soil_live_data");
+
+function field1()
+{
+  setInterval(function(){
+    fetch('https://api.thingspeak.com/channels/1833491/fields/field1/last.txt?api_key=7D92ULX6128DMIYA')
+    .then((response) => response.json())
+    .then((data) => temp_live_data.innerHTML = ' '+data+' C');
+  },2000);
+}
+
+
+function field2()
+{
+  setInterval(function(){
+    fetch('https://api.thingspeak.com/channels/1833491/fields/field2/last.txt?api_key=7D92ULX6128DMIYA')
+    .then((response) => response.json())
+    .then((data) => hum_live_data.innerHTML = ' '+data+'%');
+  },2000);
+
+}
+
+function field3()
+{
+  setInterval(function(){
+    fetch('https://api.thingspeak.com/channels/1833491/fields/field3/last.txt?api_key=7D92ULX6128DMIYA')
+    .then((response) => response.json())
+    .then((data) => soil_live_data.innerHTML = ' '+data);
+  },2000);
+
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+  field1();
+  field2();
+  field3();
+});
