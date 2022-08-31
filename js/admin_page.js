0d03441aca33ef206700f2c83f86312eda430084
rohit4242 <rohitluni4242@gmail.com>
2022-08-31 19:44:44 +0530

    init

---
+++ js/admin_page.js
@@ -0,0 +1,7 @@
+let list = [];
+let index = document.getElementById('select-crop').selectedIndex;
+
+let name = document.getElementById('select-crop').selectedIndex[selectedIndex.innerText];
+let temp = document.getElementById('var-temp').innerText;
+let hum = document.getElementById('var-soil-moisture').innerText;
+let soil = document.getElementById('var-humidity').innerText;
--- js/displayCrops.js
+++ js/displayCrops.js
@@ -21,73 +21,7 @@ const db = getDatabase(app);
 let var_temp = document.getElementById('var-temp');
 let var_soil = document.getElementById('var-soil-moisture');
 let var_hum  = document.getElementById('var-humidity');
-// console.log("hello");
 
-// let general = {
-//     temp:50,
-//     hum:20,
-//     soil:100
-// }
-
-// let cotton = {
-//     temp:20,
-//     hum:30,
-//     soil:200
-// }
-// let Ground_Nut = {
-//     temp:30,
-//     hum:40,
-//     soil:300
-// }
-
-// let Pearl_Millet = {
-//     temp:40,
-//     hum:50,
-//     soil:400
-// }
-
-// // store the crops data on to localStorage 
-// function saveCropsValue(){
-//     localStorage.setItem("general",JSON.stringify(general));
-//     localStorage.setItem("cotton",JSON.stringify(cotton));
-//     localStorage.setItem("Ground_Nut",JSON.stringify(Ground_Nut));
-//     localStorage.setItem("Pearl_Millet",JSON.stringify(Pearl_Millet));
-
-// }
-
-// function display_crops(){
-//     let s = document.getElementById('select-crop');
-//     console.log(s.value);
-//     if(s.value =="General")
-//     {
-//         var_temp.innerHTML = general.temp;
-//         var_soil.innerHTML = general.soil;
-//         var_hum.innerHTML = general.hum;
-//     }
-//     else if(s.value =="Cotton")
-//     {
-//         var_temp.innerHTML = cotton.temp;
-//         var_soil.innerHTML = cotton.soil;
-//         var_hum.innerHTML = cotton.hum;
-//     }
-//     else if(s.value =="Ground Nut")
-//     {
-//         var_temp.innerHTML = Ground_Nut.temp;
-//         var_soil.innerHTML = Ground_Nut.soil;
-//         var_hum.innerHTML = Ground_Nut.hum;
-//     }    
-//     else if(s.value =="Pearl Millet")
-//     {
-//         var_temp.innerHTML = Pearl_Millet.temp;
-//         var_soil.innerHTML = Pearl_Millet.soil;
-//         var_hum.innerHTML = Pearl_Millet.hum;
-//     }    
-//     else{
-//         console.log("Please select the crops");
-//     }
-// }
-// saveCropsValue();
-// getCropValue()
 let select_crops = document.getElementById('select-crop');
 
 function AddNewOption(nameOfCrop){
@@ -101,6 +35,7 @@ function AddAllItemToSelection(crops){
 
     crops.forEach(element => {
         AddNewOption(element.name);
+        // console.log(element.name);
     });
 }
 
@@ -113,14 +48,6 @@ function GetAllDataRealtime(){
         snapshot.forEach((childSnapshot) => {
             crops.push(childSnapshot.val());
             console.log(crops);
-            crops.forEach((c) =>{
-                select_crops.onclick = (event) =>{
-                    event.preventDefault();
-                    let index = select_crops.selectedIndex;
-                    console.log(index);
-                }
-                
-            })
           // ...
         });
         AddAllItemToSelection(crops);
