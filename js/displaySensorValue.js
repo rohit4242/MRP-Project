b8c897007c8e301d127e37bb6cda2290f86cc5fd
rohit4242 <rohitluni4242@gmail.com>
2022-10-17 14:59:33 +0530

    init

--- js/displaySensorValue.js
+++ js/displaySensorValue.js
@@ -56,35 +56,45 @@ onValue(dbref_1,(snapshot) =>{
         temp_avg_array.push(childSnapshot.val().temp);
         hum_avg_array.push(childSnapshot.val().hum);
         soil_avg_array.push(childSnapshot.val().soil_moisture);
-
+        // console.log(hum_avg_array);
         temp_avg = Average(temp_avg_array);
         hum_avg = Average(hum_avg_array);
         soil_avg = Average(soil_avg_array);
 
         temp_live_data.innerHTML = temp_avg+"&#8451;";
-        soil_live_data.innerHTML = hum_avg;
-        hum_live_data.innerHTML = soil_avg+"%";
+        soil_live_data.innerHTML = soil_avg;
+        hum_live_data.innerHTML = hum_avg+"%";
 
         console.log("Average Values");
         console.log(temp_avg);
         console.log(hum_avg);
         console.log(soil_avg);
-        for (let i = 1; i > 0; i--) {
-            let option = `<option value="${temp_avg}" selected>Average: ${temp_avg}</option>`;
-            selectSensor[0].lastElementChild.insertAdjacentHTML("afterend", option);
-        }
-        for (let i = 1; i > 0; i--) {
-            let option = `<option value="${soil_avg}" selected>Average: ${soil_avg}</option>`;
-            selectSensor[1].lastElementChild.insertAdjacentHTML("afterend", option);
-        }
-        for (let i = 1; i > 0; i--) {
-            let option = `<option value="${hum_avg}" selected>Average: ${hum_avg}</option>`;
-            selectSensor[2].lastElementChild.insertAdjacentHTML("afterend", option);
 
-        }
+        // selectSensor[0].innerHTML = "";
+        // selectSensor[1].innerHTML = "";
+        // selectSensor[2].innerHTML = "";
+        selectSensor[0].options.length = 0;
+        selectSensor[1].options.length = 0;
+        selectSensor[2].options.length = 0;
 
-    })
-})
+        selectSensor[0].options[selectSensor[0].options.length] = new Option(`Average: ${temp_avg}`,`${temp_avg}`);
+        selectSensor[1].options[selectSensor[1].options.length] = new Option(`Average: ${soil_avg}`,`${soil_avg}`);
+        selectSensor[2].options[selectSensor[2].options.length] = new Option(`Average: ${hum_avg}`,`${hum_avg}`);
+
+        // for (let i = 1; i > 0; i--) {
+        //     let option = `<option value="${temp_avg}" selected>Average: ${temp_avg}</option>`;
+        //     selectSensor[0].append(option);
+        // }
+        // for (let i = 1; i > 0; i--) {
+        //     let option = `<option value="${soil_avg}" selected>Average: ${soil_avg}</option>`;
+        //     selectSensor[1].firstChild.insertAdjacentHTML("afterend", option);
+        // }
+        // for (let i = 1; i > 0; i--) {
+        //     let option = `<option value="${hum_avg}" selected>Average: ${hum_avg}</option>`;
+        //     selectSensor[2].firstChild.insertAdjacentHTML("afterend", option);
+        // }
+    });
+});
 
 let dbref_2 = ref(db,"Sensor data");
 onValue(dbref_2, (snapshot) => {
@@ -125,5 +135,5 @@ function Average(numbers){
     for(let i = 0; i < numbers.length; i++){
         sum += numbers[i];
     }
-    return sum / numbers.length;
+    return Math.floor(sum / numbers.length);
 }
